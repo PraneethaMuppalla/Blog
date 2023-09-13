537e4efe5727abf8b0240d82384252bc3a585bc2
PraneethaMuppalla <lakshmimuppalla2453@gmail.com>
2023-09-13 23:04:39 +0530

    LogoAddition

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Header.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import placeholder from "../Images/BlogLogo.png"
 
 const Header = () => {
     return (
         <Container fluid className='main-header-flex'>
             <Row className='d-flex '>
                 <Col xs={2} className='d-flex justify-content-start'>
-                    
-                        <div className='mr-auto p-2'>Logo</div>
+                    <img  src={placeholder} alt="logo" className='h-75 w-50 rounded-circle'  />
+                
                         </Col>
                         <Col  xs={10}>
                         <div>
